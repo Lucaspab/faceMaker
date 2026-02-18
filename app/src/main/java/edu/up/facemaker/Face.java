0a718d9db5a8507e7b49ed0e89204fefce0a2b81
Lucaspab <lucaspab28@up.edu>
2026-02-18 04:23:15 -0800

    part B of the assignment for CS 301 Individual Homework
    made all the new changes, connected all the seekbars to the corresponding option clicked. made the face.

--- app/src/main/java/edu/up/facemaker/Face.java
+++ app/src/main/java/edu/up/facemaker/Face.java
@@ -11,22 +11,63 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
 import java.util.Random;
+import android.graphics.RectF;
 
 
 public class Face extends SurfaceView{
-    private Paint magenta = new Paint();
+
+    // declare all of the paints that will be used
+    private Paint skin = new Paint();
+    private Paint eyes = new Paint();
+    private Paint hair = new Paint();
+    private Paint smile = new Paint();
+    // used to create the smile for the face
+    RectF mouth = new RectF(400f, 400f, 600f, 700f);
+
+
+    // instance variables for the skin,eye,hair color and hair style
+    private int skinColor;
+    private int eyeColor;
+    private int hairColor;
+    private int hairStyle;
 
     public Face(Context context, AttributeSet attrs){
         super(context, attrs);
         setWillNotDraw(false);
-        magenta.setColor(Color.MAGENTA);
+        randomize();
+
     }
+    @Override
+    public void onDraw(Canvas canvas) {
+        // make the background white
+        super.onDraw(canvas);
+        //setting the colors to each corresponding attribute
+        skin.setColor(skinColor);
+        eyes.setColor(eyeColor);
+        hair.setColor(hairColor);
 
-    int skinColor = 0;
-    int eyeColor = 0;
-    int hairColor = 0;
-    int hairStyle = 0;
+        // creating the head shape of the face
+        canvas.drawCircle(500,500,400, skin);
 
+        //creating the mouth of the face
+        smile.setColor(Color.BLACK);
+        canvas.drawArc(mouth, 0, 180, false, smile);
+
+        // giving color to the eyes and creating them
+        canvas.drawCircle(350,400,60, eyes);
+        canvas.drawCircle(650,400,60, eyes);
+
+        // creating a hairstyle and setting them from 0 to 2 for a total of 3
+        if(hairStyle == 0){
+            // nothing, since were making it bald
+        }
+        else if(hairStyle == 1){
+            canvas.drawRect(400,50,600,300, hair);
+        }
+        else{
+            canvas.drawRect(150,50,850,300,hair);
+        }
+    }
 
     // created a constructor where we have it randomize all the features of the face
     public void randomize(){
@@ -44,10 +85,61 @@ public class Face extends SurfaceView{
 
         // hairstyle is different since we use a spinner and have 3 options well use random variable to randomize the options from 1-3
         hairStyle = rand.nextInt(3);
+        invalidate();
+    }
+    // creating setters for each method so they
+
+    // having this constructor set skin color
+    public void setSkinColor(int colorSkin){
+        skinColor = colorSkin;
+        invalidate();
     }
 
-    @Override
-    public void onDraw(Canvas canvas) {
-        canvas.drawCircle(500.0f, 500.0f, 400.0f, magenta);
+    // have the color of the eyes be set
+    public void setEyesColor(int color){
+        eyeColor = color;
+        invalidate();
+    }
+    // have the hair color be set
+    public void setHairColor(int color){
+        hairColor = color;
+        invalidate();
+    }
+
+    public void setHairStyle(int style){
+        hairStyle = style;
+        invalidate();
+    }
+    // now getter methods for each method so it can be used in main activity
+    // have a skin getter method
+    public int getSkinColor(){
+        return skinColor;
+    }
+    // make the eyeColor accescible
+    public int getEyeColor(){
+        return eyeColor;
+    }
+    // give hair color a getter method
+    public int getHairColor(){
+        return hairColor;
+    }
+    // same here giving a getter method to the hair style
+    public int getHairStyle(){
+        return hairStyle;
+    }
+    // helper method for the color changes depending on what was picked, the type so like eyes hair and skin
+    public void colorUpdate(int red, int green , int blue, int type){
+        int color = Color.rgb(red,green,blue);
+        if(type == 0){
+            hairColor = color;
+        }
+        else if(type == 1){
+            eyeColor = color;
+        }
+        else{
+            skinColor = color;
+        }
+        // very important so it can redraw
+        invalidate();
     }
 }
--- app/src/main/java/edu/up/facemaker/MainActivity.java
+++ app/src/main/java/edu/up/facemaker/MainActivity.java
@@ -4,33 +4,153 @@
  */
 package edu.up.facemaker;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     //These are the values for the Department Spinner
     String[] hairStyle = {
             "Bald",
             "Mohawk",
             "Long",
     };
+    // have the face class be set here
     private Face myFace;
+    // instance variables for RGB seekbars
+    private SeekBar seekRed, seekGreen, seekBlue;
+    // instance variable for radioButtons
+    private RadioGroup group;
+    //instance variable for spinner
+    private Spinner spinner;
+    // instance variable for button
+    private Button randButton;
+    // the type so like the eyes hair etc.
+    private int typePicked = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.face);
         myFace = findViewById(R.id.view);
         myFace.invalidate();
+
+        //Initialize the seekbars of RGB
+        seekRed = findViewById(R.id.redSeek);
+        seekGreen = findViewById(R.id.greenSeek);
+        seekBlue = findViewById(R.id.blueSeek);
+        // Initialize radio buttons
+        group = findViewById(R.id.radiogroup);
+        // Initialize random Button
+        randButton = findViewById(R.id.RandomFace);
+        //have the face start up randomly
+        myFace.randomize();
+        //have the spinner initialize
+        spinner = findViewById(R.id.spinner);
+        // calling all the helper methods i wrote
+        randButton();
+        setSeekBars();
+        radioButtons();
+        helperSpinner();
+        changeColorSB(myFace.getHairColor());
+
+    }
+
+    // helper for spinner
+    public void helperSpinner(){
         //Initialize the Department Spinner
-        Spinner deptSpinner = findViewById(R.id.spinner);
         ArrayAdapter<String> hairAdapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 this.hairStyle);
-        deptSpinner.setAdapter(hairAdapter);
+        spinner.setAdapter(hairAdapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // No action needed when no selection is made
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        myFace.setHairStyle(position);
+    }
+
+    //helper method for radio buttons
+    public void radioButtons(){
+        group.setOnCheckedChangeListener((group, Id)->{
+            if(Id == R.id.Hair) {
+                typePicked = 0;
+                changeColorSB(myFace.getHairColor());
+            }
+            else if(Id == R.id.Eyes){
+                typePicked = 1;
+                changeColorSB(myFace.getEyeColor());
+            }
+            else if(Id == R.id.Skin){
+                typePicked = 2;
+                changeColorSB(myFace.getSkinColor());
+            }
+
+        });
+    }
+    //have a helper for teh random button face
+    public void randButton() {
+        // have the button when clicked randomly create a face, here have the spinner select a hairstyle
+        // and have an if statement for the color on each seekbar
+        randButton.setOnClickListener(v -> { myFace.randomize(); spinner.setSelection(myFace.getHairStyle());
+        if(typePicked == 0) {
+            changeColorSB(myFace.getHairColor());
+        }
+        else if(typePicked == 1) {
+            changeColorSB(myFace.getEyeColor());
+        }
+        else {
+            changeColorSB(myFace.getSkinColor());
+            }
+        });
+    }
+
+    public void setSeekBars(){
+        SeekBar.OnSeekBarChangeListener listener = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar sb, int value, boolean fromUser) {
+                myFace.colorUpdate(
+                        seekRed.getProgress(),
+                        seekGreen.getProgress(),
+                        seekBlue.getProgress(),
+                        typePicked
+                );
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
+
+        seekRed.setOnSeekBarChangeListener(listener);
+        seekGreen.setOnSeekBarChangeListener(listener);
+        seekBlue.setOnSeekBarChangeListener(listener);
+
+    }
+    public void changeColorSB(int color){
+        seekRed.setProgress(Color.red(color));
+        seekGreen.setProgress(Color.green(color));
+        seekBlue.setProgress(Color.blue(color));
     }
 
 }
